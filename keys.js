1b8b0bcb3bf27c3c4f5d56c4a6b69ae7a63a7a91
matt fyke <mfyke@live.com>
2018-11-07 15:34:09 -0800

    twitter and spotify functions done

--- keys.js
+++ keys.js
@@ -1,6 +1,10 @@
 exports.twitterKeys = {
   consumer_key: 'bEN5vYn7tsNOLzJmT2lfSwjMx',
   consumer_secret: '8eyXEugtxw3qUazDQrom6V01KigVuPthrij3p4khyB1dod7NKm',
-  access_token_key: '848764433245405188-ryZjYC4Lh4aQlt03BaUkfemQ3pVATuO',
-  access_token_secret: 'aquzzfWVclpSyR6EELKfZDjoJOwa0jKiZkojJJM6xpsnG',
+  access_token_key: '848764433245405188-YgQM6RrcCY6yLEobPXbtsO5UtGnv2zN',
+  access_token_secret: 'g8KXe8Y65odqV5LukW9uR5PwGS72RpBXvohDDUzMMUZOY',
+}
+exports.spotifyKeys = {
+	id: '5c52c8731768406cb856c4b7f923e14d',
+	secret: '210e611288634296bfff78b6ca4cb194'
 }
--- liri.js
+++ liri.js
@@ -2,11 +2,70 @@ var keys = require("./keys.js");
 var Twitter = require("twitter");
 var client = new Twitter (keys.twitterkeys);
 var request = require("request");
-var spotify = require("spotify");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify (keys.spotifyKeys);
 var fs = require("fs");
 var func = process.argv[2];
 var search="";
 for(var i=3; i<process.argv.length;i++) {
 	search+=process.argv[i] + " ";
 }
-console.log(search);
+
+switch(func) {
+	// if myTweets is typed in use twitter npm package to make a twitter api call and get the tweets, then display them in the console
+	case "myTweets":
+		console.log("tweets showing");
+		var params = {screen_name: 'ShreksDawg', count: "20"};
+		client.get('statuses/user_timeline', params, function(error, tweets, response) {
+			console.log(error);
+  			if (!error) {
+  				for(var i=0;i<tweets.length;i++){
+    			console.log(tweets[i].text);
+    			}
+  			}
+		});
+		break;
+	// if spotifyThisSong is typed then use spotify npm package to make a spotify api call with the name of the song
+	case "spotifyThisSong":
+		console.log("spotifying this song");
+		if (search == "") {
+			spotify.search({ type: 'track', query: "The Sign" }, function(err, data) {
+	    		if ( err ) {
+	        		console.log('Error occurred: ' + err);
+	        		return;
+	    		}
+	 		
+		    	// Do something with 'data'
+
+		    	console.log(data.tracks.items[8].artists[0].name);
+		    	console.log(data.tracks.items[8].name);
+		    	console.log(data.tracks.items[8].preview_url);
+		    	console.log(data.tracks.items[8].album.name);
+			});
+		}
+		else{
+			spotify.search({ type: 'track', query: search }, function(err, data) {
+	    		if ( err ) {
+	        		console.log('Error occurred: ' + err);
+	        		return;
+	    		}
+	 		
+	    	// Do something with 'data'
+	    	console.log(data.tracks.items[0].artists[0].name);
+	    	console.log(data.tracks.items[0].name);
+	    	console.log(data.tracks.items[0].preview_url);
+	    	console.log(data.tracks.items[0].album.name);
+
+			});
+		}
+		break;
+	case "movieThis":
+		console.log("movie this initiated!");	
+		break;
+	case "doWhatItSays":
+		console.log("doing what it says");
+		break;
+	default:
+		console.log("what is this command? Try one of these: myTweets, spotifyThisSong + the song name, movieThis + the movie name, or doWhatItSays for a random command!");
+		break;	
+}
